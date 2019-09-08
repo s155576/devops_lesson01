76829930be7599c861288fa2fe4d1c87586dd967
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-09-08 17:34:28 +0200

    Setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.File;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir("temp");
+        String port = System.getenv("PORT");
+
+        if (port == null) {
+            port="8080";
+        }
+
+        tomcat.setPort(Integer.parseInt(port));
+        tomcat.getConnector();
+
+        tomcat.addWebapp("/", new File("src/main/webapp").getAbsolutePath());
+
+        tomcat.start();
+
+        tomcat.getServer().await();
     }
 }
