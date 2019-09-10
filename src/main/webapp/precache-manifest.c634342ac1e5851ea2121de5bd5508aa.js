06b6f501dbafb4953ca1bdc3f7e09d8f3a685d94
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-09-10 10:30:26 +0200

    Copy to main/webapp

---
+++ src/main/webapp/precache-manifest.c634342ac1e5851ea2121de5bd5508aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f8267e23d5c233efe4c4306f1a1d58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ecdab125bf1469aa652",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e1d9cc27791ac614d15",
+    "url": "/static/js/2.8ce7f97d.chunk.js"
+  },
+  {
+    "revision": "0ecdab125bf1469aa652",
+    "url": "/static/js/main.fd76b8ed.chunk.js"
+  },
+  {
+    "revision": "faf3d913dea1519336c1",
+    "url": "/static/js/runtime~main.0b761a9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
